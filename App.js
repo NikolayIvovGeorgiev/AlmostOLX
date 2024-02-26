7353c68a02e602ba37c110aa7848ee31ef5d58cb
unknown <nikolay.ivov.georgiev@gmail.com>
2024-02-26 15:14:47 +0200

    Cards component

--- App.js
+++ App.js
@@ -4,9 +4,26 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import LoginButton from "./app/components/LoginButton";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for sale"
+        subtitle="100$"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
+
+  // <WelcomeScreen />;
 
   // <LoginButton />;
   // <ViewImageScreen />;
