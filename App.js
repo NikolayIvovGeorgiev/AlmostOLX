ba1e04357783f5b2710945eb16587c3a842553fa
unknown <nikolay.ivov.georgiev@gmail.com>
2024-03-20 16:29:25 +0200

    Permission for library

--- App.js
+++ App.js
@@ -1,61 +1,25 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { ImageComponent, View, StyleSheet, TextInput } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import LoginButton from "./app/components/LoginButton";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import { StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
 import colors from "./app/config/colors";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import { useState } from "react";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect } from "react";
 
 export default function App() {
-  const [category, setCategory] = useState();
-
-  // const items = [
-  //   {
-  //     icon: "",
-  //     label: "1",
-  //     value: "sad",
-  //   },
-  //   {
-  //     icon: "",
-  //     label: "2",
-  //     value: "sasdaad",
-  //   },
-  //   {
-  //     icon: "",
-  //     label: "3",
-  //     value: "sa123123d",
-  //   },
-  // ];
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("дай позволение бе");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
   return (
     <GestureHandlerRootView style={styles.outerLayer}>
-      <Screen>
-        <ListingEditScreen />
-        {/* <ListItem
-          title="Niki G"
-          subTitle="react-native god"
-          image={require("./app/assets/NG.jpg")}
-          showChevrons={true}
-        /> */}
-      </Screen>
+      <Screen></Screen>
     </GestureHandlerRootView>
   );
-  // <GestureHandlerRootView>{<MessagesScreen />}</GestureHandlerRootView>;
 }
 const styles = StyleSheet.create({
   outerLayer: {
